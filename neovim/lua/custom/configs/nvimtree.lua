1b060d84614c7d0cab280c83f2d50c07f9e1101b
krsma33 <krsma33@gmail.com>
2023-11-30 22:58:28 +0100

    Updated nvim filtering

--- neovim/lua/custom/configs/nvimtree.lua
+++ neovim/lua/custom/configs/nvimtree.lua
@@ -4,6 +4,21 @@ M.opts = {
   view = {
     width = 45,
   },
+  filters = {
+    git_ignored = true,
+    dotfiles = true,
+    -- custom = {
+    --   "bin",
+    --   "obj",
+    -- },
+    exclude = {
+      vim.fn.stdpath "config" .. "\\lua\\custom",
+      ".gitignore",
+    },
+  },
+  git = {
+    enable = true,
+  },
 }
 
 return M
