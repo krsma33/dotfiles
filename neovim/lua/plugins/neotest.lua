7e27bb6389eb83589cb7cf2869de1fbe8b22e2d5
krsma <stefan.krsmanovic@arcadis.com>
2024-03-10 22:47:36 +0100

    disable neotest discovery for performance

--- neovim/lua/plugins/neotest.lua
+++ neovim/lua/plugins/neotest.lua
@@ -15,11 +15,13 @@ return {
           -- Tell neotest-dotnet to use either solution (requires .sln file) or project (requires .csproj or .fsproj file) as project root
           -- Note: If neovim is opened from the solution root, using the 'project' setting may sometimes find all nested projects, however,
           --       to locate all test projects in the solution more reliably (if a .sln file is present) then 'solution' is better.
-          discovery_root = "solution",
+          discovery_root = "project",
         },
       },
+      discovery = {
+        enabled = false,
+      },
       status = { virtual_text = true },
-      output = { open_on_run = true },
       quickfix = {
         open = function()
           if require("lazyvim.util").has("trouble.nvim") then
