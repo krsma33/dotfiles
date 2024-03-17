e82ba53cae3d3f4023ed9b8a0c6eb734dd370004
krsma <krsma33@gmail.com>
2024-03-17 12:59:09 +0100

    Updated lualine theme a bit

--- neovim/lua/lualine/themes/leaf-custom.lua
+++ neovim/lua/lualine/themes/leaf-custom.lua
@@ -5,32 +5,37 @@ local leaf = {}
 leaf.normal = {
   a = { bg = colors.blue0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.blue1 },
-  c = { bg = colors.bg0, fg = colors.fg2 },
+  c = { bg = colors.bg0, fg = colors.blue1 },
 }
 
 leaf.insert = {
   a = { bg = colors.green0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.green1 },
+  c = { bg = colors.bg0, fg = colors.green1 },
 }
 
 leaf.command = {
   a = { bg = colors.yellow0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.yellow1 },
+  c = { bg = colors.bg0, fg = colors.yellow1 },
 }
 
 leaf.visual = {
   a = { bg = colors.purple0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.purple1 },
+  c = { bg = colors.bg0, fg = colors.purple1 },
 }
 
 leaf.replace = {
   a = { bg = colors.red0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.red1 },
+  c = { bg = colors.bg0, fg = colors.red1 },
 }
 
 leaf.terminal = {
   a = { bg = colors.teal0, fg = colors.bg0 },
   b = { bg = colors.bg0, fg = colors.teal1 },
+  c = { bg = colors.bg0, fg = colors.teal1 },
 }
 
 leaf.inactive = {
--- neovim/lua/plugins/ui/lualine.lua
+++ neovim/lua/plugins/ui/lualine.lua
@@ -1,5 +1,32 @@
+local Util = require("lazyvim.util")
+
 return function(_, opts)
+  local icons = require("lazyvim.config").icons
+
   opts.options = {
     theme = "leaf-custom",
+    component_separators = { left = "", right = "" },
+  }
+
+  opts.sections.lualine_b = {
+    { "branch" },
+    Util.lualine.root_dir(),
+    {
+      "diagnostics",
+      symbols = {
+        error = icons.diagnostics.Error,
+        warn = icons.diagnostics.Warn,
+        info = icons.diagnostics.Info,
+        hint = icons.diagnostics.Hint,
+      },
+    },
+    { Util.lualine.pretty_path() },
+    { "encoding", separator = "", padding = { left = 1, right = 0 } },
+    "fileformat",
+  }
+
+  opts.sections.lualine_c = {
+    { "%=", separator = "" },
+    { "filetype", icon_only = true },
   }
 end
