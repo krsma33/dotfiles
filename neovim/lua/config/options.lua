d3f0f7808cb075f3766bf8a5da6323cb186a6fe6
krsma <stefan.krsmanovic@arcadis.com>
2024-03-23 00:13:30 +0100

    Changed Neotest highlight groups to be inline with theme

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -4,8 +4,15 @@
 
 local path = require("utils.path")
 
+-- Flash
 vim.api.nvim_set_hl(0, "FlashLabelCustom", { bg = "#700060", fg = "#c8d3f5" })
+-- Cmp
 vim.api.nvim_set_hl(0, "CmpDocumentationCustom", { bg = "#1d1c1d" })
+-- NeoTest
+vim.api.nvim_set_hl(0, "NeotestAdapterName", { link = "PreProc" })
+vim.api.nvim_set_hl(0, "NeotestDir", { link = "Directory" })
+vim.api.nvim_set_hl(0, "NeotestFile", { link = "Type" })
+vim.api.nvim_set_hl(0, "NeotestNamespace", { link = "Keyword" })
 
 if path.is_windows then
   vim.opt.shell = "pwsh"
