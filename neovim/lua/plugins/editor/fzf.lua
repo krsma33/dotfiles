a483fdac1986e2a6671c710af962646dc9c50a9b
krsma <krsma33@gmail.com>
2024-12-15 11:18:33 +0100

    Cleanup

--- neovim/lua/plugins/editor/fzf.lua
+++ neovim/lua/plugins/editor/fzf.lua
@@ -2,4 +2,5 @@ return function (_, opts)
   local config = require("fzf-lua.config")
   config.defaults.keymap.builtin["<M-p>"] = "toggle-preview"
   opts.preview_opts = "hidden"
+  opts.winopts.preview.horizontal = "right:50%"
 end
