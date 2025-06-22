26f5febe864ee0fbae54f5a50761099c5f5df6cb
krsma <stefan.krsmanovic@arcadis.com>
2025-06-22 19:45:56 +0200

    Adapt to roslyn changes

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -62,3 +62,35 @@ vim.diagnostic.config({
     current_line = true,
   }
 })
+
+-- Lsp Configuration
+vim.lsp.config("*", {
+    capabilities = vim.lsp.protocol.make_client_capabilities()
+})
+
+vim.lsp.config("roslyn", {
+    settings = {
+      ["csharp|inlay_hints"] = {
+        csharp_enable_inlay_hints_for_implicit_object_creation = true,
+        csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+        csharp_enable_inlay_hints_for_types = true,
+        dotnet_enable_inlay_hints_for_indexer_parameters = true,
+        dotnet_enable_inlay_hints_for_literal_parameters = true,
+        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+        dotnet_enable_inlay_hints_for_other_parameters = true,
+        dotnet_enable_inlay_hints_for_parameters = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+      },
+      ["csharp|code_lens"] = {
+        dotnet_enable_references_code_lens = true,
+      },
+      ["csharp|background_analysis"] = {
+        dotnet_analyzer_diagnostics_scope = "openFiles",
+        dotnet_compiler_diagnostics_scope = "fullSolution",
+      },
+    },
+})
+
